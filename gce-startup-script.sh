c15d9b89cc9ca671511fa958f642bbbe187ed065
Mitchell, David <david.mitchell@gft.com>
2019-06-21 11:09:02 +0100

    commiting

--- gce-startup-script.sh
+++ gce-startup-script.sh
@@ -5,4 +5,4 @@ VARIABLE2="c.NotebookApp.ip = '0.0.0.0'"
 echo $VARIABLE | cat - .jupyter/jupyter_notebook_config.py > temp && mv temp .jupyter/jupyter_notebook_config.py
 echo $VARIABLE2 | cat - .jupyter/jupyter_notebook_config.py > temp2 && mv temp2 .jupyter/jupyter_notebook_config.py
 jupyter notebook --no-browser --port 8888
-eche 'push5'
+eche 'push6'
